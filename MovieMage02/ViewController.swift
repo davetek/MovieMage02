fc67678828c444c927a91c97a882688bd6c77d94
davetek <dlawrence4994@gmail.com>
2020-08-22 11:01:06 -0400

    Initial project setup

---
+++ MovieMage02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieMage02
+//
+//  Created by David Lawrence on 8/22/20.
+//  Copyright © 2020 clarity for action. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
